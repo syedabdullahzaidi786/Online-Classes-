7cb4eb952e884679422551eb58828417e1dbc820
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2025-03-24 15:50:03 +0500

    updated

--- Class 01/main.ts
+++ Class 01/main.ts
@@ -1,3 +1,26 @@
 // console.log("Hello World");
 // console.log('Hello Maaz');
 
+// let meessage = 'Hello World';
+// meessage = 'Hello Maaz';
+// console.log(meessage);
+
+// const message = 'Hello World';  // const is used to declare a constant variable
+//  message = 'Hello Maaz';  // This will give an error because we can't change the value of a constant variable
+// console.log(message);
+
+// Syntax Error:
+// constt message = 'Hello World'; // Spelling mistake in const
+// console.log(message);
+
+// Assignability Error:
+
+// let message: number = 123;
+// message = 321;
+// console.log(message);
+
+// Data Types in TypeScript
+// 1. Number  1234567890
+// 2. String "?" Letters/ Words/ Sentences/ Paragraphs/ Alphabets/ Numbers
+// 3. Boolean true/false
+
