7610d16946280297a99e3154322629ad63f9f850
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2025-03-25 14:41:28 +0500

    updated

--- Class 02/main.js
+++ Class 02/main.js
@@ -5,9 +5,29 @@
 // lett Age = 30;
 // console.log(Age); // Error: Cannot find name 'lett'.
 // Strong Typing
-var Age = 30;
+// let Num1: any = 10;
+// Num1 = "Hello"; // Error: Type 'string' is not assignable to type 'number'.
+// console.log(Num1);
+// // Object 
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+// console.log(person.name);
+// console.log(person.age); 
+// let student = {
+//     Name: "John",
+//     Age: 30,
+//     Class: "10th",
+//     Roll: 20
+// }
+// console.log(student.Name);
+// console.log(student.Age); 
+// console.log(student.Class);
+// console.log(student.Roll); // Error: Unexpected token. Did you mean to use a semicolon instead of a colon? 
+// Cases:
+// Upper Case  
+// Lower Case
+// Camel Case
 var Name = "John";
-var Class = "TypeScript";
-var isUpdated = true;
-console.log(Name, Age, Class, isUpdated); // John 30 TypeScript true
-// console.log(Name,Age,Class); // 30
+console.log(Name.toUpperCase());
--- Class 02/main.ts
+++ Class 02/main.ts
@@ -15,3 +15,34 @@
 // console.log(Num1);
 
 
+// // Object 
+// let person = {
+//     name: "John",
+//     age: 30
+// };
+// console.log(person.name);
+// console.log(person.age); 
+
+
+// let student = {
+//     Name: "John",
+//     Age: 30,
+//     Class: "10th",
+//     Roll: 20
+// }
+// console.log(student.Name);
+// console.log(student.Age); 
+// console.log(student.Class);
+// console.log(student.Roll); // Error: Unexpected token. Did you mean to use a semicolon instead of a colon? 
+
+
+// Cases:
+// Upper Case  // All Capital Letters
+// Lower Case // All Small Letters
+// Camel Case // First letter of each word is capital except the first word
+
+// let Name : string = "John";
+// console.log(Name.toUpperCase());
+
+let StudentRollNumber = "John"; // Camel Case
+console.log(StudentRollNumber.toLowerCase());
