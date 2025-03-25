fb9ad296e02fd4b6f137abe1681a5ecd1ec1d626
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2025-03-25 14:13:28 +0500

    Class 02 Added

---
+++ Class 02/main.js
@@ -0,0 +1,13 @@
+// // Type Error
+// let Name = "John";
+// console.loger(Name); // Error: Cannot find name 'consoleer'.
+// syntax error
+// lett Age = 30;
+// console.log(Age); // Error: Cannot find name 'lett'.
+// Strong Typing
+var Age = 30;
+var Name = "John";
+var Class = "TypeScript";
+var isUpdated = true;
+console.log(Name, Age, Class, isUpdated); // John 30 TypeScript true
+// console.log(Name,Age,Class); // 30
---
+++ Class 02/main.ts
@@ -0,0 +1,17 @@
+// // Type Error
+
+// let Name = "John";
+// console.loger(Name); // Error: Cannot find name 'consoleer'.
+
+// syntax error
+// lett Age = 30;
+// console.log(Age); // Error: Cannot find name 'lett'.
+
+
+// Strong Typing
+
+// let Num1: any = 10;
+// Num1 = "Hello"; // Error: Type 'string' is not assignable to type 'number'.
+// console.log(Num1);
+
+
