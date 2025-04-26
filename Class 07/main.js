d270467e16adfbe420fa698fc1563e8b2128777e
syedabdullahzaidi786 <syedabdullahzaidi786@gmail.com>
2025-04-26 22:43:11 +0500

    new update

---
+++ Class 07/main.js
@@ -0,0 +1,6 @@
+var student = {
+    name: "Maaz Rehan",
+    age: 14,
+    major: "Computer Science"
+};
+console.log(student);
---
+++ Class 07/main.ts
@@ -0,0 +1,21 @@
+// // Objects in TypeScript
+// // Objects are a collection of key-value pairs
+
+// let student = {
+//     name: "Maaz Rehan",
+//     age: 14,
+//     major: "Computer Science"
+// }
+// console.log(student);
+
+let student: {
+    name: string,
+    age: number,
+    major: string
+} = {
+        name: "Maaz Rehan",
+        age: 14,
+        major: "Computer Science"
+}
+console.log(student);
+
